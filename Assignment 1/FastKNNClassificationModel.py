ca559f0b508aeb6bf5c33e7ab4f69c61f7ed9395
Zwukii <alen.smlatic2002@gmail.com>
2026-04-03 20:13:05 +0200

    Done with fastKNN model

--- Assignment 1/FastKNNClassificationModel.py
+++ Assignment 1/FastKNNClassificationModel.py
@@ -1,34 +1,37 @@
 from scipy.spatial import KDTree
-
 import numpy as  np
 
 
-class FastKNNRegressionModel(MachineLearningModel):
-    """
-    Class for fast KNN regression model using KDTree.
-    """
-
+class FastKNNClassificationModel():
     def __init__(self, k):
-        """
-        Initialize the model with the specified instructions.
-
-        Parameters:
-        k (int): Number of neighbors.
-        """
         self.k = k
         self.X_train = None
         self.y_train = None
-        self.tree = None
+        self.kdTree = None
 
+   
     def fit(self, X, y):
         self.X_train = np.array(X)
         self.y_train = np.array(y)
-        self.tree =KDTree(self.X_train)
+        self.kdTree = KDTree(self.X_train)
         return None
         
 
     def predict(self, X):
-        pass
+        predictions =  []
+        count_data = {}
+        for x in X:
+            distance, indices = self.tree.query(x, k=self.k)
+            y_value = self.y_train[indices]
+            for y in y_value:
+                if y in count_data:
+                    count_data[y] += 1
+                else:
+                    count_data[y] = 1
+
+            prediction = max(count_data, key=count_data.get)
+            predictions.append(prediction)
+        return np.array(predictions)
 
     def evaluate(self, y_true, y_predicted):
-        pass
+        return np.mean(y_true == y_predicted)
